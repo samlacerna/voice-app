0c502ebd786fad8a076779c0612354f7181bc250
Samantha Lacerna <socialmediasamm@gmail.com>
2017-05-25 16:40:45 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
 
     // MongoDB connection string - MONGO_URL is for local dev,
     // MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
-    mongoUrl: 'mongodb://samlacerna:admin1@ds133271.mlab.com:33271/voice-app'
+    mongoUrl: 'confidential'
 };
